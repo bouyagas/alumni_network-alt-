d6bab804ca6c1e95cbc277a438ed06935ddf6a6e
mohamed gassama <bouyagassama@gmail.com>
2020-01-30 11:40:34 -0500

    styling the component

--- dist/serverConfig/testing.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = {
-    mongoDbUrl: 'mongodb://localhost:27017/alumni_network',
-    secrets: {
-        jwt: 'devSecret',
-    },
-};
-//# sourceMappingURL=testing.js.map
